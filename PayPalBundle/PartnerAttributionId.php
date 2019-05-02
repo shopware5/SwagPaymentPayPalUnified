0a4728dd443597d2c90fe1892e98b8029cdf63ed
Michael Telgmann <mt@shopware.com>
2019-05-02 15:30:54 +0200

    NTR - Update BN codes

--- PayPalBundle/PartnerAttributionId.php
+++ PayPalBundle/PartnerAttributionId.php
@@ -11,22 +11,22 @@ namespace SwagPaymentPayPalUnified\PayPalBundle;
 class PartnerAttributionId
 {
     /**
-     * Shopware Partner Id for PayPal Classic or Express-Checkout
+     * Shopware Partner Id for PayPal Classic
      */
-    const PAYPAL_CLASSIC = 'Shopware_Cart_EC_2';
+    const PAYPAL_CLASSIC = 'Shopware_Cart_EC_5native';
 
     /**
      * Shopware Partner Id for PayPal Plus
      */
-    const PAYPAL_PLUS = 'Shopware_Cart_Plus_2';
+    const PAYPAL_PLUS = 'Shopware_Cart_Plus_5native';
 
     /**
      * Shopware Partner Id for PayPal Installments
      */
-    const PAYPAL_INSTALLMENTS = 'Shopware_Cart_Inst_2';
+    const PAYPAL_INSTALLMENTS = 'Shopware_Cart_Rate_5native';
 
     /**
-     * Shopware Partner Id for PayPal Express Checkout with full page redirect
+     * Shopware Partner Id for PayPal Express Checkout
      */
-    const PAYPAL_EXPRESS_CHECKOUT = 'Shopware_Cart_ECS_2';
+    const PAYPAL_EXPRESS_CHECKOUT = 'Shopware_Cart_ECS_5native';
 }
