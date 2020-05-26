829711dab29b9efb0f64ff4967851c6450210363
Michael Telgmann <mt@shopware.com>
2020-05-26 12:26:14 +0200

    PT-11491 - Add PayPal cookies to consent manager

---
+++ Subscriber/CookieConsent.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types=1);
+/**
+ * (c) shopware AG <info@shopware.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SwagPaymentPayPalUnified\Subscriber;
+
+use Enlight\Event\SubscriberInterface;
+use Shopware\Bundle\CookieBundle\CookieCollection;
+use Shopware\Bundle\CookieBundle\Structs\CookieGroupStruct;
+use Shopware\Bundle\CookieBundle\Structs\CookieStruct;
+use Shopware_Components_Snippet_Manager as SnippetManager;
+
+class CookieConsent implements SubscriberInterface
+{
+    /**
+     * @var SnippetManager
+     */
+    private $snippetManager;
+
+    public function __construct(SnippetManager $snippetManager)
+    {
+        $this->snippetManager = $snippetManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'CookieCollector_Collect_Cookies' => 'addPayPalCookie',
+        ];
+    }
+
+    /**
+     * @return CookieCollection
+     */
+    public function addPayPalCookie()
+    {
+        $collection = new CookieCollection();
+        $collection->add(new CookieStruct(
+            'paypal-cookies',
+            // PayPal Cookies are not handled by Shopware, because they are not set by the plugin.
+            // Therefore we use a regex which should not affect other cookies
+            '/^paypal-cookie-consent-manager$/',
+            $this->snippetManager->getNamespace('frontend/paypal_unified/cookie_consent/cookie')->get('cookie/label'),
+            CookieGroupStruct::TECHNICAL
+        ));
+
+        return $collection;
+    }
+}
