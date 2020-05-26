5e70feb6713e8ac29888b65e29e0e7c8f5e2cb68
Michael Telgmann <mt@shopware.com>
2020-05-26 16:43:13 +0200

    PT-11491 - Fix compatibility to older Shopware versions

--- Resources/views/frontend/_public/src/js/jquery.swag-paypal-unified.installments-banner.js
+++ Resources/views/frontend/_public/src/js/jquery.swag-paypal-unified.installments-banner.js
@@ -1,4 +1,4 @@
-;(function($, window, undefined) {
+;(function($, window, document, undefined) {
     'use strict';
 
     $.plugin('swagPayPalUnifiedInstallmentsBanner', {
@@ -78,6 +78,11 @@
                 return;
             }
 
+            this.checkGetCookiePreference();
+            if (this.isPayPalAllowed() === false) {
+                return;
+            }
+
             this.applyDataAttributes();
             $.publish('plugin/swagPayPalUnifiedInstallmentsBanner/init', this);
 
@@ -104,6 +109,43 @@
             }).render(this.$el.get(0));
         },
 
+        checkGetCookiePreference: function() {
+            if ($.isFunction($.getCookiePreference)) {
+                return;
+            }
+
+            // Polyfill for older shopware versions
+            $.getCookiePreference = function() {
+                return false;
+            };
+        },
+
+        isPayPalAllowed: function() {
+            var me = this;
+
+            me.cookieValue = me.getCookie();
+
+            return me.cookieValue || $.getCookiePreference('paypal-cookies');
+        },
+
+        getCookie: function() {
+            var name = "allowCookie=",
+                decodedCookie = decodeURIComponent(document.cookie),
+                cookieArray = decodedCookie.split(';');
+
+            for (var i = 0; i < cookieArray.length; i++) {
+                var cookie = cookieArray[i];
+                while (cookie.charAt(0) === ' ') {
+                    cookie = cookie.substring(1);
+                }
+                if (cookie.indexOf(name) === 0) {
+                    return cookie.substring(name.length, cookie.length);
+                }
+            }
+
+            return null;
+        },
+
         destroy: function() {
             this._destroy();
         }
@@ -114,4 +156,4 @@
     });
 
     window.StateManager.addPlugin('*[data-paypalUnifiedInstallmentsBanner="true"]', 'swagPayPalUnifiedInstallmentsBanner');
-})(jQuery, window);
+})(jQuery, window, document);
