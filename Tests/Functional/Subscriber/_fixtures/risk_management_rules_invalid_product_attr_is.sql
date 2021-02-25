91f348089000953bd37205df81a064b3af47a204
Dennis Garding <di@shopware.com>
2021-02-25 16:02:58 +0100

    PT-12286 - Fix risk manangement attribute rules

---
+++ Tests/Functional/Subscriber/_fixtures/risk_management_rules_invalid_product_attr_is.sql
@@ -0,0 +1,12 @@
+INSERT INTO `swag_payment_paypal_unified_settings_general` (`id`, `shop_id`, `active`, `client_id`, `client_secret`, `sandbox`, `show_sidebar_logo`, `brand_name`, `landing_page_type`, `send_order_number`, `order_number_prefix`, `use_in_context`, `log_level`, `display_errors`, `advertise_returns`, `use_smart_payment_buttons`, `merchant_location`, `submit_cart`) VALUES
+(1,	1,	1,	'Foo',	'Bar',	1,	0,	'',	'Login',	0,	'',	1,	0,	0,	0,	0,	'germany',	1);
+
+SET @paypalpamentid = (SELECT `id` FROM s_core_paymentmeans WHERE `name`='SwagPaymentPayPalUnified');
+
+INSERT INTO `s_core_rulesets` (`id`, `paymentID`, `rule1`, `value1`, `rule2`, `value2`)
+VALUES (80, @paypalpamentid, 'ATTRIS', 'invalidAttr|2', '', '');
+
+UPDATE s_articles_attributes
+SET attr1 = 2
+WHERE id = 429;
+
