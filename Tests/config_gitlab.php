76354188e818a8d278e3fad3a7a68bc70a9fbe84
Michael Telgmann <mt@shopware.com>
2022-04-06 16:54:35 +0200

    PT-12692 - Fix shop registration so it is only used on relevant actions

---
+++ Tests/config_gitlab.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * (c) shopware AG <info@shopware.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'db' => [
+        'username' => 'root',
+        'password' => 'root',
+        'dbname' => 'shopware',
+        'host' => 'mysql',
+        'port' => '3306',
+    ],
+
+    'csrfProtection' => [
+        'backend' => false,
+    ],
+
+    'backendsession' => [
+        'unitTestEnabled' => true,
+    ],
+];
