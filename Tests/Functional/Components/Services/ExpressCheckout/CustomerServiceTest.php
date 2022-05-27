0a8f40cf31f3d5e009ede28302dbc32e0c0e416e
Michael Telgmann <mt@shopware.com>
2022-05-27 10:55:25 +0200

    PT-11456 - Consider name of shipping address if provided during Express Checkout

--- Tests/Functional/Components/Services/ExpressCheckout/CustomerServiceTest.php
+++ Tests/Functional/Components/Services/ExpressCheckout/CustomerServiceTest.php
@@ -10,14 +10,13 @@ namespace SwagPaymentPayPalUnified\Tests\Functional\Components\Services\ExpressC
 
 use PHPUnit\Framework\TestCase;
 use SwagPaymentPayPalUnified\Components\Services\ExpressCheckout\CustomerService;
-use SwagPaymentPayPalUnified\PayPalBundle\Structs\Common\Address;
-use SwagPaymentPayPalUnified\PayPalBundle\Structs\Payment\Payer\PayerInfo;
 use SwagPaymentPayPalUnified\PayPalBundle\V2\Api\Order;
 use SwagPaymentPayPalUnified\PayPalBundle\V2\Api\Order\Payer;
+use SwagPaymentPayPalUnified\PayPalBundle\V2\Api\Order\Payer\Address as PayerAddress;
 use SwagPaymentPayPalUnified\PayPalBundle\V2\Api\Order\Payer\Name;
 use SwagPaymentPayPalUnified\PayPalBundle\V2\Api\Order\Payer\Phone;
 use SwagPaymentPayPalUnified\PayPalBundle\V2\Api\Order\Payer\Phone\PhoneNumber;
-use SwagPaymentPayPalUnified\PayPalBundle\V2\Api\Order\PurchaseUnit\Payee;
+use SwagPaymentPayPalUnified\PayPalBundle\V2\Api\Order\PurchaseUnit;
 use SwagPaymentPayPalUnified\PayPalBundle\V2\Api\Order\PurchaseUnit\Shipping;
 use SwagPaymentPayPalUnified\PayPalBundle\V2\Api\Order\PurchaseUnit\Shipping\Address as ShippingAddress;
 use SwagPaymentPayPalUnified\PayPalBundle\V2\Api\Order\PurchaseUnit\Shipping\Name as ShippingName;
@@ -31,120 +30,93 @@ class CustomerServiceTest extends TestCase
     use ContainerTrait;
     use ShopRegistrationTrait;
 
-    public function testServiceIsAvailable()
-    {
-        $service = $this->getContainer()->get('paypal_unified.express_checkout.customer_service');
-        static::assertSame(CustomerService::class, \get_class($service));
-    }
+    const STREET = 'Teststreet 1a';
+    const ADDRESSLINE_2 = 'Basement';
+    const POSTAL_CODE = '48624';
+    const CITY = 'Schöppingen';
+    const COUNTRY_CODE = 'DE';
+    const STATE_CODE = 'NW';
+    const GIVEN_NAME = 'GivenName';
+    const SURNAME = 'Surname';
+    const FULL_NAME_1 = 'Different';
+    const FULL_NAME_2 = 'Name Test';
+    const CUSTOMER_EMAIL_ADDRESS = 'phpunit@test.com';
 
-    public function testConstruct()
+    /**
+     * @dataProvider createNewCustomerCases
+     *
+     * @param bool   $purchaseUnitAvailable
+     * @param string $expectedFirstName
+     * @param string $expectedLastName
+     *
+     * @return void
+     */
+    public function testCreateNewCustomer($purchaseUnitAvailable, $expectedFirstName, $expectedLastName)
     {
-        $service = new CustomerService(
-            $this->getContainer()->get('config'),
-            $this->getContainer()->get('dbal_connection'),
-            $this->getContainer()->get('shopware.form.factory'),
-            $this->getContainer()->get('shopware_storefront.context_service'),
-            $this->getContainer()->get('shopware_account.register_service'),
-            $this->getContainer()->get('front'),
-            $this->getContainer()->get('paypal_unified.dependency_provider'),
-            $this->getContainer()->get('paypal_unified.payment_method_provider'),
-            $this->getContainer()->get('paypal_unified.logger_service')
-        );
+        $payer = $this->createPayer();
 
-        static::assertNotNull($service);
-    }
-
-    public function testCreateNewCustomer()
-    {
         $orderStruct = new Order();
-
-        $payer = new Payer();
-        $payerInfo = new PayerInfo();
-
-        $name = new Name();
-        $name->setGivenName('Shopware');
-        $name->setSurname('PHPUnit');
-
-        $phoneNumber = new PhoneNumber();
-        $phoneNumber->setNationalNumber('0123456789');
-
-        $phone = new Phone();
-        $phone->setPhoneNumber($phoneNumber);
-
-        $payer->setName($name);
-        $payer->setPhone($phone);
-        $payer->setEmailAddress('phpunit@test.com');
-        $payer->setPayerId('TestUser');
-
-        $billingAddress = new Address();
-        $billingAddress->setCountryCode('DE');
-        $billingAddress->setState('NW');
-        $billingAddress->setPhone('0123456789');
-        $billingAddress->setLine1('Teststreet 1a');
-        $billingAddress->setLine2('Basement');
-        $billingAddress->setPostalCode('48624');
-        $billingAddress->setCity('Schöppingen');
-
-        $payerInfo->setBillingAddress($billingAddress);
         $orderStruct->setPayer($payer);
 
-        $displayData = new Payee\DisplayData();
+        if ($purchaseUnitAvailable) {
+            $purchaseUnit = new PurchaseUnit();
+            $shipping = $this->createShipping();
+            $purchaseUnit->setShipping($shipping);
+            $orderStruct->setPurchaseUnits([$purchaseUnit]);
+        }
 
-        $payee = new Payee();
-        $payee->setEmailAddress('phpunit@test.com');
-        $payee->setDisplayData($displayData);
-
-        $shippingName = new ShippingName();
-        $shippingName->setFullName('Shopware PHPUnit');
-
-        $shippingAddress = new ShippingAddress();
-        $shippingAddress->setCountryCode($billingAddress->getCountryCode());
-        $shippingAddress->setPostalCode($billingAddress->getPostalCode());
-        $shippingAddress->setAddressLine1($billingAddress->getLine1());
-        $shippingAddress->setAddressLine2($billingAddress->getLine2());
-        $shippingAddress->setAdminArea2($billingAddress->getCity());
-
-        $shipping = new Shipping();
-        $shipping->setName($shippingName);
-        $shipping->setAddress($shippingAddress);
-
-        $purchaseUnit = new Order\PurchaseUnit();
-        $purchaseUnit->setPayee($payee);
-        $purchaseUnit->setShipping($shipping);
-
-        $service = $this->getCustomerService();
-
-        $orderStruct->setPurchaseUnits([$purchaseUnit]);
         $this->setFrontRequest();
-        $service->createNewCustomer($orderStruct);
 
-        $user = $this->getUserByMail()[0];
+        $this->getCustomerService()->createNewCustomer($orderStruct);
 
-        static::assertNotNull($user);
-        static::assertSame('1', $user['accountmode']);
-        static::assertSame('Shopware', $user['firstname']);
-        static::assertSame('PHPUnit', $user['lastname']);
+        $customer = $this->getCustomerByMail(self::CUSTOMER_EMAIL_ADDRESS);
+
+        static::assertNotNull($customer);
+        static::assertSame('1', $customer['accountmode']);
+        static::assertSame($expectedFirstName, $customer['firstname']);
+        static::assertSame($expectedLastName, $customer['lastname']);
 
         static::assertNotNull($this->getContainer()->get('session')->get('sUserId'));
     }
 
     /**
+     * @return \Generator<string, array<bool|string>>
+     */
+    public function createNewCustomerCases()
+    {
+        yield 'purchase unit available' => [
+            true,
+            self::FULL_NAME_1,
+            self::FULL_NAME_2,
+        ];
+
+        yield 'purchase unit not available' => [
+            false,
+            self::GIVEN_NAME,
+            self::SURNAME,
+        ];
+    }
+
+    /**
      * @param string $mail
      *
-     * @return array<array<string, mixed>>
+     * @return array<string, mixed>
      */
-    private function getUserByMail($mail = 'phpunit@test.com')
+    private function getCustomerByMail($mail)
     {
         $db = $this->getContainer()->get('dbal_connection');
 
-        $sql = 'SELECT * FROM s_user WHERE email=:emailAddress';
+        $sql = 'SELECT * FROM s_user WHERE email LIKE :emailAddress';
 
-        return $db->fetchAll($sql, ['emailAddress' => $mail]);
+        return $db->fetchAll($sql, ['emailAddress' => $mail])[0];
     }
 
+    /**
+     * @return void
+     */
     private function setFrontRequest()
     {
-        Shopware()->Front()->setRequest(new \Enlight_Controller_Request_RequestTestCase());
+        $this->getContainer()->get('front')->setRequest(new \Enlight_Controller_Request_RequestTestCase());
     }
 
     /**
@@ -153,5 +125,88 @@ class CustomerServiceTest extends TestCase
     private function getCustomerService()
     {
         return $this->getContainer()->get('paypal_unified.express_checkout.customer_service');
+    }
+
+    /**
+     * @return Payer
+     */
+    private function createPayer()
+    {
+        $name = $this->createName();
+        $phone = $this->createPhone();
+        $address = $this->cratePayerAddress();
+
+        $payer = new Payer();
+        $payer->setName($name);
+        $payer->setPhone($phone);
+        $payer->setAddress($address);
+        $payer->setEmailAddress(self::CUSTOMER_EMAIL_ADDRESS);
+        $payer->setPayerId('TestUser');
+
+        return $payer;
+    }
+
+    /**
+     * @return Name
+     */
+    private function createName()
+    {
+        $name = new Name();
+        $name->setGivenName(self::GIVEN_NAME);
+        $name->setSurname(self::SURNAME);
+
+        return $name;
+    }
+
+    /**
+     * @return Phone
+     */
+    private function createPhone()
+    {
+        $phoneNumber = new PhoneNumber();
+        $phoneNumber->setNationalNumber('0123456789');
+
+        $phone = new Phone();
+        $phone->setPhoneNumber($phoneNumber);
+
+        return $phone;
+    }
+
+    /**
+     * @return PayerAddress
+     */
+    private function cratePayerAddress()
+    {
+        $address = new PayerAddress();
+        $address->setAddressLine1(self::STREET);
+        $address->setAddressLine2(self::ADDRESSLINE_2);
+        $address->setPostalCode(self::POSTAL_CODE);
+        $address->setAdminArea2(self::CITY);
+        $address->setCountryCode(self::COUNTRY_CODE);
+        $address->setAdminArea1(self::STATE_CODE);
+
+        return $address;
+    }
+
+    /**
+     * @return Shipping
+     */
+    private function createShipping()
+    {
+        $shippingName = new ShippingName();
+        $shippingName->setFullName(sprintf('%s %s', self::FULL_NAME_1, self::FULL_NAME_2));
+
+        $shippingAddress = new ShippingAddress();
+        $shippingAddress->setCountryCode(self::COUNTRY_CODE);
+        $shippingAddress->setPostalCode(self::POSTAL_CODE);
+        $shippingAddress->setAddressLine1(self::STREET);
+        $shippingAddress->setAddressLine2(self::ADDRESSLINE_2);
+        $shippingAddress->setAdminArea2(self::CITY);
+
+        $shipping = new Shipping();
+        $shipping->setName($shippingName);
+        $shipping->setAddress($shippingAddress);
+
+        return $shipping;
     }
 }
